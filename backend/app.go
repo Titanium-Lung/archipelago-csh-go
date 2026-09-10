12566ab80ebd1726228c6e96746d413cef3b7082
Titanium-Lung <cooper@psychofx.com>
2026-09-09 22:38:41 -0400

    Backend can talk to frontend Also Docker things and gin

--- backend/app.go
+++ backend/app.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":5001", nil))
+type User struct {
+	UUID     string `json:"uuid"`
+	Username string `json:"username"`
+	Picture  string `json:"picture_url"`
+	CSH      bool   `json:"csh"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!")
+var user User
+
+func getUser(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, user)
+}
+
+func main() {
+	user.UUID = "5"
+	user.Username = "lung"
+	user.Picture = "https://profiles.csh.rit.edu/image/lung"
+	user.CSH = true
+
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.GET("/api/user", getUser)
+
+	router.Run(":5001")
 }
